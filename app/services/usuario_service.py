6a3f11a7f87f17affb9ae725c138307c91f8ed31
Caio Pinheiro <86423541+caiofpinheiro@users.noreply.github.com>
2024-10-28 12:16:23 +0000

    add usuario service

---
+++ app/services/usuario_service.py
@@ -0,0 +1,28 @@
+
+from models.usuario_model import Usuario
+from repositories.usuario_repository import UsuarioRepository
+
+
+class UsuarioService:
+    def __init__(self, repository: UsuarioRepository) -> None:
+        self.repository = repository
+
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try:
+            usuario = Usuario (nome=nome, email=email, senha=senha)
+
+            consulta_usuario = self.repository.listar_todos_usuarios_por_email(usuario.email)
+
+            if consulta_usuario:
+                print("Uusário já existe no banco de dados.")
+                return
+
+            self.repository.salvar_usuario(usuario)
+            print("Usuário salvo com sucesso.")
+        except TypeError as erro:
+            print(f"Erro ao salvar usuário: {erro}")
+        except Exception as erro:
+            print(f"Ocorreu um erro inesperado: {erro}")
+
+    def listar_todos_usuarios(self):
+        return self.repository.listar_todos_usuarios()
